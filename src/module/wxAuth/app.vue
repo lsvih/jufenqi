1a8913bbf582454383573afed6ec763fa02f2e95
lyz <408772145@qq.com>
2016-10-23 13:45:48 +0800

    ...

--- src/module/wxAuth/app.vue
+++ src/module/wxAuth/app.vue
@@ -1,4 +1,5 @@
 <template>
+
 </template>
 
 <script>
@@ -6,28 +7,37 @@ import Lib from 'assets/Lib.js'
 export default {
   data() {
     return {
-      lastUrl:Lib.M.GetRequest().url,
-      code:Lib.M.GetRequest().code,
-      appId:"wxe1d923f20f84fac5"
+      lastUrl: Lib.M.GetRequest().url,
+      code: Lib.M.GetRequest().code,
+      appId: "wxe1d923f20f84fac5"
     }
   },
-  ready(){
-    if(Lib.M.isAuth()){
+  ready() {
+    if (Lib.M.isAuth()) {
       location.href = lastUrl
-    }else{
+    } else {
       this.getCode()
     }
   },
   methods: {
-    getCode(){
-      if(this.code){
-        this.$http.post(`${Lib.C.authApi}loginUsingWechat`,{body:{code:this.code}},{xhr: {withCredentials: true}}),then((res)=>{
+    getCode() {
+      if (this.code) {
+        this.$http.post(`${Lib.C.authApi}loginUsingWechat`, {
+          body: {
+            code: this.code
+          }
+        }, {
+          xhr: {
+            withCredentials: true
+          },
+          emulateJSON: true
+        }), then((res) => {
           console.log(res)
-        },(res)=>{
+        }, (res) => {
           alert("微信登录失败，请稍后重试")
           location.href = `./index.html`
         })
-      }else{
+      } else {
         location.href = `https://open.weixin.qq.com/connect/oauth2/authorize?appid=${this.appId}&redirect_uri=${location.href}&response_type=code&scope=snsapi_userinfo&state=STATE#wechat_redirec`
       }
     }
