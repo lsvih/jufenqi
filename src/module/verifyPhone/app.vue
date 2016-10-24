9473406a87fc260fe6fa0e0f5151ad9849e4c40b
lyz <408772145@qq.com>
2016-10-24 11:54:31 +0800

    Add phone number reg

--- src/module/verifyPhone/app.vue
+++ src/module/verifyPhone/app.vue
@@ -1,8 +1,9 @@
 <template>
 <div class="verify-phone">
   <div class="user-icon"><img :src="user.profile.profileImage"></div>
-  <div class="phone-number"><img src="./login.png"><input type="tel" placeholder="请输入手机号码"></div>
-
+  <div class="phone-number"><img src="./login.png"><input type="tel" placeholder="请输入手机号码" v-model="phoneNumber"></div>
+  <div class="user-agreement">我已阅读并同意<b>居分期用户协议</b></div>
+  <div class="submit" v-bind:class="{'active':isTruePhoneNum()}">绑   定</div>
 </div>
 </template>
 
@@ -12,7 +13,8 @@ import Lib from 'assets/Lib.js'
 export default {
   data() {
     return {
-      user: JSON.parse(localStorage.getItem("user"))
+      user: JSON.parse(localStorage.getItem("user")),
+      phoneNumber: "",
     }
   },
   components: {
@@ -23,11 +25,10 @@ export default {
 
   },
   methods: {
-
-
-
-
-
+    isTruePhoneNum() {
+      let reg = /^1[3|4|5|8]\d{9}$/
+      return reg.test(String(this.phoneNumber))
+    }
   }
 }
 </script>
@@ -37,56 +38,83 @@ body {
     background-color: #eee;
     position: relative;
 }
-.user-icon{
-  position: absolute;
-  width: 80px;
-  height: 80px;
-  border-radius: 40px;
-  top:40px;
-  left:50%;
-  margin-left: -40px;
-  overflow: hidden;
-  img{
-    height: 100%;
+.user-icon {
+    position: absolute;
+    width: 80px;
+    height: 80px;
+    border-radius: 40px;
+    top: 40px;
+    left: 50%;
+    margin-left: -40px;
+    overflow: hidden;
+    img {
+        height: 100%;
+        width: 100%;
+    }
+}
+.user-name {
+    position: absolute;
     width: 100%;
-  }
-}
-.user-name{
-  position: absolute;
-  width: 100%;
-  height: 16px;
-  line-height: 16px;
-  color:#393939;
-  left:0;
-  top:130px;
-}
-.phone-number{
-  position: absolute;
-  border-radius: 5px;
-  border:1px solid #d8d8d8;
-  background-color: #fff;
-  height: 44px;
-  width: calc( ~"100% - 30px" );
-  top:206px;
-  left:15px;
-  img{
-    position: absolute;
-    width: 20px;
-    height: 24px;
-    top:11px;
-    left:15px;
-  }
-  input{
-    position: absolute;
-    border:0;
-    background-color: #fff;
-    top: 14px;
-    left:59px;
-    padding:0;
     height: 16px;
     line-height: 16px;
-    font-size: 16px;
+    color: #393939;
+    left: 0;
+    top: 130px;
+}
+.phone-number {
+    position: absolute;
+    border-radius: 5px;
+    border: 1px solid #d8d8d8;
+    background-color: #fff;
+    height: 44px;
+    width: calc( ~"100% - 30px" );
+    top: 206px;
+    left: 15px;
+    img {
+        position: absolute;
+        width: 20px;
+        height: 24px;
+        top: 11px;
+        left: 15px;
+    }
+    input {
+        position: absolute;
+        border: 0;
+        background-color: #fff;
+        top: 14px;
+        left: 59px;
+        padding: 0;
+        height: 16px;
+        line-height: 16px;
+        font-size: 16px;
+        color: #999;
+    }
+}
+.user-agreement {
+    position: absolute;
+    width: calc(~"100% - 30px");
+    height: 12px;
+    line-height: 12px;
+    left: 15px;
+    top: 260px;
     color: #999;
-  }
+    font-size: 12px;
+    b {
+        font-weight: normal;
+        color: #3BA794;
+    }
+}
+.submit {
+    border-radius: 5px;
+    background-color: #e2e2e2;
+    font-size: 16px;
+    font-weight: bold;
+    height: 44px;
+    width: calc(~"100% - 30px");
+    left: 15px;
+    top: 374px;
+    .active {
+        background-color: #88C928;
+    }
 }
 </style>
