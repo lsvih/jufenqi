4cba73783aa5120519abacf0a33c8624efe2b5bd
lyz <408772145@qq.com>
2016-10-24 11:11:11 +0800

    Add style

--- src/module/verifyPhone/app.vue
+++ src/module/verifyPhone/app.vue
@@ -1,13 +1,9 @@
 <template>
-
-
-<div class="login" id="login" >
-this is login page
+<div class="verify-phone">
+  <div class="user-icon"><img :src="user.profile.profileImage"></div>
+  <div class="phone-number"><input type="tel" placeholder="请输入手机号码"></div>
 
 </div>
-
-
-
 </template>
 
 <script>
@@ -16,14 +12,13 @@ import Lib from 'assets/Lib.js'
 export default {
   data() {
     return {
-    	appname:Lib.C.appname,
-
+      user: JSON.parse(localStorage.getItem("user"))
     }
   },
   components: {
 
   },
-  ready(){
+  ready() {
 
 
   },
@@ -37,6 +32,51 @@ export default {
 }
 </script>
 
-<style scoped>
-
+<style scoped lang="less">
+body {
+    background-color: #eee;
+    position: relative;
+}
+.user-icon{
+  position: absolute;
+  width: 80px;
+  height: 80px;
+  top:40px;
+  left:50%;
+  margin-left: -40px;
+  img{
+    height: 100%;
+    width: 100%;
+  }
+}
+.user-name{
+  position: absolute;
+  width: 100%;
+  height: 16px;
+  line-height: 16px;
+  color:#393939;
+  left:0;
+  top:130px;
+}
+.phone-number{
+  position: absolute;
+  border-radius: 5px;
+  border:1px solid #999;
+  background-color: #fff;
+  height: 44px;
+  width: calc( ~"100%-30px" );
+  top:206px;
+  left:15px;
+  input{
+    position: absolute;
+    border:0;
+    background-color: #fff;
+    top: 14px;
+    left:50px;
+    height: 16px;
+    line-height: 16px;
+    font-size: 16px;
+    color: #999;
+  }
+}
 </style>
