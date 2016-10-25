6d3bc632ad350c56b97ac1af2ac9c5d10e83bc3f
lyz <408772145@qq.com>
2016-10-25 13:57:11 +0800

    Add select payments

--- src/module/zc-order/app.vue
+++ src/module/zc-order/app.vue
@@ -2,8 +2,6 @@
 <header>
   <img src="status.png">
   <div class="status">{{zcStatusList[status].name}}</div>
-  <div class="btn" v-if="status==0">取消订单</div>
-
 </header>
 <div class="butler">
   <div class="zc-butler-img"><img :src="butlerImg"></div>
@@ -23,16 +21,29 @@
       </cell>
       <div class="line-2">
         <div class="line-2-title">正价总额</div>
-        <div class="line-2-right">{{shop.zjPrice|currency "￥" 2}}</div>
+        <div class="line-2-right" style="color:rgb(255, 204, 102);">{{shop.zjPrice|currency "￥" 2}}</div>
       </div>
       <div class="line-2">
         <div class="line-2-title">特价总额</div>
-        <div class="line-2-right">{{shop.tjPrice|currency "￥" 2}}</div>
+        <div class="line-2-right" style="color:#88C929;">{{shop.tjPrice|currency "￥" 2}}</div>
       </div>
       <div class="line-2" style="border-top:5px solid #eee!important;">
-          <div class="line-2-title">总额</div>
-          <div class="line-2-right">{{shop.zjPrice+shop.tjPrice|currency "￥" 2}}</div>
+        <div class="line-2-title">总额</div>
+        <div class="line-2-right">{{shop.zjPrice+shop.tjPrice|currency "￥" 2}}</div>
       </div>
+      <div class="line-3" v-if="status !== 2&&status !== 3">
+        <div class="cancel">{{status === 0?"取消预约":(status === 1?"取消订单":"申请退款")}}</div>
+      </div>
+    </group>
+    <group v-if="status === 1">
+      <div class="line-2">
+        <div class="line-2-title">请选择您的购买方式</div>
+        <div class="line-2-right"><img class="down" src="./down.png"></div>
+      </div>
+      <radio  :options="payments" @on-change="selectPay"></radio>
+    </group>
+    <group v-if="status === 1">
+      <div class="sumbit-order" :class="{'active':payWay!==''}">确认订单</div>
     </group>
   </div>
 </div>
@@ -42,6 +53,7 @@
 import Lib from 'assets/Lib.js'
 import Group from 'vux-components/group'
 import Cell from 'vux-components/cell'
+import Radio from 'vux-components/radio'
 export default {
   data() {
     return {
@@ -94,6 +106,17 @@ export default {
         status: 4,
         name: "已收货"
       }],
+      payments:[
+        {
+          key:'0',
+          value:'全款购买'
+        },
+        {
+          key:'1',
+          value:'分期购买'
+        }
+      ],
+      payWay:""
     }
   },
   ready() {
@@ -102,8 +125,12 @@ export default {
   components: {
     Group,
     Cell,
+    Radio
   },
   methods: {
+    selectPay(e){
+      this.payWay = this.payments[Number(e)]
+    }
   }
 }
 </script>
@@ -180,18 +207,6 @@ header {
         font-size: 12px;
         color: #393939;
     }
-    .btn {
-        position: absolute;
-        right: 15px;
-        top: 5px;
-        width: 60px;
-        height: 20px;
-        border: 1px solid #393939;
-        font-size: 12px;
-        color: #393939;
-        line-height: 20px;
-        text-align: center;
-    }
 }
 .butler {
     height: 50px;
@@ -244,72 +259,110 @@ header {
             width: 100%;
         }
     }
-    .line-1{
-      width: 100%;
-      height: 50px;
-      position: relative;
-      .shop-name{
-        position: absolute;
-        left:15px;
-        top:17px;
-        font-size: 16px;
-        color:#393939;
-      }
-      .btn{
-        position: absolute;
-        top:15px;
-        right:15px;
-        font-size: 12px;
-        color: #fff;
-        width: 50px;
-        height: 20px;
-        line-height: 20px;
-        text-align: center;
-        background-color: #88C929;
-        border-radius: 2px;
-      }
-    }
-    .zc-cell{
-      height: 10px;
-      position: relative;
-      .zc-name{
-        position: absolute;
-        top:9px;
-        left:15px;
-        font-size: 12px;
-        color:#393939;
-      }
-      .zc-price{
-        position: absolute;
-        top:9px;
-        right:15px;
-        font-size: 12px;
-        color:#EC5835;
-        span{
-          margin-left:10px;
-          color:#999;
+    .line-1 {
+        width: 100%;
+        height: 50px;
+        position: relative;
+        .shop-name {
+            position: absolute;
+            left: 15px;
+            top: 17px;
+            font-size: 16px;
+            color: #393939;
         }
-      }
+        .btn {
+            position: absolute;
+            top: 15px;
+            right: 15px;
+            font-size: 12px;
+            color: #fff;
+            width: 50px;
+            height: 20px;
+            line-height: 20px;
+            text-align: center;
+            background-color: #88C929;
+            border-radius: 2px;
+        }
     }
-    .line-2{
-      height: 44px;
-      width: 100%;
-      position: relative;
-      border-top: 1px solid #eee;
-      .line-2-title{
-        position: absolute;
-        top: 16px;
-        left:15px;
-        font-size: 12px;
-        color:#999;
-      }
-      .line-2-right{
-        position: absolute;
-        top:16px;
-        right:15px;
-        font-size: 12px;
-        color:#EC5835;
-      }
+    .zc-cell {
+        height: 10px;
+        position: relative;
+        .zc-name {
+            position: absolute;
+            top: 9px;
+            left: 15px;
+            font-size: 12px;
+            color: #393939;
+        }
+        .zc-price {
+            position: absolute;
+            top: 9px;
+            right: 15px;
+            font-size: 12px;
+            color: #EC5835;
+            span {
+                margin-left: 10px;
+                color: #999;
+            }
+        }
     }
+    .line-2 {
+        height: 44px;
+        width: 100%;
+        position: relative;
+        border-top: 1px solid #eee;
+        .line-2-title {
+            position: absolute;
+            top: 0;
+            height:44px;
+            line-height: 44px;
+            left: 15px;
+            font-size: 12px;
+            color: #999;
+        }
+        .line-2-right {
+            position: absolute;
+            top: 0;
+            height: 44px;
+            line-height: 44px;
+            right: 15px;
+            font-size: 12px;
+            color: #EC5835;
+            .down{
+              position: absolute;
+              height: 10px;
+              width: 17px;
+              right: 0;
+              top:17px;
+            }
+        }
+    }
+    .line-3 {
+        height: 30px;
+        width: 100%;
+        position: relative;
+        border-top: 1px solid #eee;
+        .cancel {
+            position: absolute;
+            right: 15px;
+            top: 0;
+            height: 30px;
+            font-size: 12px;
+            line-height: 30px;
+            color: #999;
+        }
+    }
+}
+.sumbit-order{
+  height: 44px;
+  width: 100%;
+  line-height: 44px;
+  background-color: #e2e2e2;
+  text-align: center;
+  font-size: 16px;
+  color:#fff;
+}
+.active {
+    background-color: #9EBC2B!important;
 }
 </style>
