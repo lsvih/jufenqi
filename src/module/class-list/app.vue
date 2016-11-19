b31caf0f60ad848aa90bf4c8b25393a0bef21c85
lyz <408772145@qq.com>
2016-11-19 17:03:03 +0800

    update

--- src/module/class-list/app.vue
+++ src/module/class-list/app.vue
@@ -1,31 +1,228 @@
 <template>
-<x-img v-for="class in classList" :src="img+class.showImg" class="class-item" :style="{width:(getScreenWidth()-41)/2 + 'px',height:(getScreenWidth()-41)/2 /167*100 + 'px'}" v-tap="gotoClass(class.id)">
-</x-img>
+<div class="menu">
+  <div class="menu-item" v-for="parent of classList" :class="{'active':select === $index}" v-tap="select = $index">
+    <div class="active-border" v-if="select === $index"></div>
+    <div class="parent-name">{{parent.name}}</div>
+  </div>
+</div>
+<div class="content">
+  <div class="content-item" v-for="class of classList[select].child" v-tap="gotoClass(class.id)"><img :src="class.img"><div class="class-name">{{class.name}}</div></div>
+</div>
 </template>
 
 <script>
 import Lib from 'assets/Lib.js'
-import XImg from 'vux-components/x-img'
 import axios from 'axios'
 export default {
   data() {
     return {
-      classList: [],
+      classList: [{
+        name: '家电',
+        id: 1,
+        child: [{
+          name: '电视',
+          id: 1,
+          img: 'http://placekitten.com/500/500'
+        }, {
+          name: '冰箱',
+          id: 2,
+          img: 'http://placekitten.com/500/500'
+        }]
+      }, {
+        name: '家电',
+        id: 1,
+        child: [{
+          name: '电视',
+          id: 1,
+          img: 'http://placekitten.com/500/500'
+        }, {
+          name: '冰箱',
+          id: 2,
+          img: 'http://placekitten.com/500/500'
+        },{
+          name: '电视',
+          id: 1,
+          img: 'http://placekitten.com/500/500'
+        }, {
+          name: '冰箱',
+          id: 2,
+          img: 'http://placekitten.com/500/500'
+        },{
+          name: '电视',
+          id: 1,
+          img: 'http://placekitten.com/500/500'
+        }, {
+          name: '冰箱',
+          id: 2,
+          img: 'http://placekitten.com/500/500'
+        },{
+          name: '电视',
+          id: 1,
+          img: 'http://placekitten.com/500/500'
+        }, {
+          name: '冰箱',
+          id: 2,
+          img: 'http://placekitten.com/500/500'
+        },{
+          name: '电视',
+          id: 1,
+          img: 'http://placekitten.com/500/500'
+        }, {
+          name: '冰箱',
+          id: 2,
+          img: 'http://placekitten.com/500/500'
+        },{
+          name: '电视',
+          id: 1,
+          img: 'http://placekitten.com/500/500'
+        }, {
+          name: '冰箱',
+          id: 2,
+          img: 'http://placekitten.com/500/500'
+        },{
+          name: '电视',
+          id: 1,
+          img: 'http://placekitten.com/500/500'
+        }, {
+          name: '冰箱',
+          id: 2,
+          img: 'http://placekitten.com/500/500'
+        },{
+          name: '电视',
+          id: 1,
+          img: 'http://placekitten.com/500/500'
+        }, {
+          name: '冰箱',
+          id: 2,
+          img: 'http://placekitten.com/500/500'
+        },{
+          name: '电视',
+          id: 1,
+          img: 'http://placekitten.com/500/500'
+        }, {
+          name: '冰箱',
+          id: 2,
+          img: 'http://placekitten.com/500/500'
+        },{
+          name: '电视',
+          id: 1,
+          img: 'http://placekitten.com/500/500'
+        }, {
+          name: '冰箱',
+          id: 2,
+          img: 'http://placekitten.com/500/500'
+        },{
+          name: '电视',
+          id: 1,
+          img: 'http://placekitten.com/500/500'
+        }, {
+          name: '冰箱',
+          id: 2,
+          img: 'http://placekitten.com/500/500'
+        },{
+          name: '电视',
+          id: 1,
+          img: 'http://placekitten.com/500/500'
+        }, {
+          name: '冰箱',
+          id: 2,
+          img: 'http://placekitten.com/500/500'
+        },{
+          name: '电视',
+          id: 1,
+          img: 'http://placekitten.com/500/500'
+        }, {
+          name: '冰箱',
+          id: 2,
+          img: 'http://placekitten.com/500/500'
+        },{
+          name: '电视',
+          id: 1,
+          img: 'http://placekitten.com/500/500'
+        }, {
+          name: '冰箱',
+          id: 2,
+          img: 'http://placekitten.com/500/500'
+        },{
+          name: '电视',
+          id: 1,
+          img: 'http://placekitten.com/500/500'
+        }, {
+          name: '冰箱',
+          id: 2,
+          img: 'http://placekitten.com/500/500'
+        },{
+          name: '电视',
+          id: 1,
+          img: 'http://placekitten.com/500/500'
+        }, {
+          name: '冰箱',
+          id: 2,
+          img: 'http://placekitten.com/500/500'
+        },{
+          name: '电视',
+          id: 1,
+          img: 'http://placekitten.com/500/500'
+        }, {
+          name: '冰箱',
+          id: 2,
+          img: 'http://placekitten.com/500/500'
+        },{
+          name: '电视',
+          id: 1,
+          img: 'http://placekitten.com/500/500'
+        }, {
+          name: '冰箱',
+          id: 2,
+          img: 'http://placekitten.com/500/500'
+        },{
+          name: '电视',
+          id: 1,
+          img: 'http://placekitten.com/500/500'
+        }, {
+          name: '冰箱',
+          id: 2,
+          img: 'http://placekitten.com/500/500'
+        }]
+      }, {
+        name: '家电',
+        id: 1,
+        child: [{
+          name: '电视',
+          id: 1,
+          img: 'http://placekitten.com/500/500'
+        }, {
+          name: '冰箱',
+          id: 2,
+          img: 'http://placekitten.com/500/500'
+        }]
+      }, {
+        name: '家电',
+        id: 1,
+        child: [{
+          name: '电视',
+          id: 1,
+          img: 'http://placekitten.com/500/500'
+        }, {
+          name: '冰箱',
+          id: 2,
+          img: 'http://placekitten.com/500/500'
+        }]
+      }],
+      select: 0,
       img: Lib.C.imgUrl
     }
   },
-  components: {
-    XImg
-  },
-  ready(){
-    axios.get(`${Lib.C.merApi}categories`, {
-      params: {size:1000}
-    }).then((res) => {
-      this.classList = res.data.data
-    }).catch((res) => {
-      alert("网络连接失败，请刷新重试")
-      window.location.reload()
-    })
+  components: {},
+  ready() {
+    // axios.get(`${Lib.C.merApi}categories`, {
+    //   params: {size:1000}
+    // }).then((res) => {
+    //   this.classList = res.data.data
+    // }).catch((res) => {
+    //   alert("网络连接失败，请刷新重试")
+    //   window.location.reload()
+    // })
   },
   methods: {
     getScreenWidth() {
@@ -44,14 +241,64 @@ body {
 }
 </style>
 <style scoped lang="less">
-.class-item {
-    margin-top: 11px;
+.menu {
+    position: absolute;
+    top: 0;
+    left: 0;
+    overflow-x: hidden;
+    overflow-y: scroll;
+    height: 100%;
+    width: 81px;
+    .menu-item {
+        position: relative;
+        height: 44px;
+        width: calc(~"100% - 1px");
+        border-bottom: 1px solid #e5e5e5;
+        border-right: 1px solid #e5e5e5;
+        background-color: #f8f8f8;
+        font-size: 12px;
+        text-align: center;
+        line-height: 45px;
+        color:#393939;
+    }
+    .active{
+      color:#f5a623;
+      background-color: #fff;
+    }
+    .active-border{
+      position: absolute;
+      left: 0;
+      top:0;
+      width: 3px;
+      height: 100%;
+      background-color: #f5a623;
+    }
 }
-.class-item:nth-child(odd) {
-    margin-left: 15px;
-    margin-right: 11px;
-}
-.class-item:nth-child(even) {
-    margin-right: 15px;
+.content{
+  position: absolute;
+  top:0;
+  right: 0;
+  overflow-x: hidden;
+  overflow-y: scroll;
+  height: calc(~"100% - 16px");
+  width: calc(~"100% - 81px - 18px");
+  padding: 16px 9px;
+  .content-item{
+    height: 100px;
+    width: 78px;
+    float: left;
+    margin: 0 7px 20px 7px;
+    img{
+      height: 78px;
+      width: 78px;
+    }
+    .class-name{
+      height: 12px;
+      width: 78px;
+      text-align: center;
+      color: #999;
+      font-size: 12px;
+    }
+  }
 }
 </style>
