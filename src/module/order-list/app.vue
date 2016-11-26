c3e6249c08d725dbd2e428a4b4bc32ad08789773
lyz <408772145@qq.com>
2016-11-26 12:02:07 +0800

    Change all order item to common component

--- src/module/order-list/app.vue
+++ src/module/order-list/app.vue
@@ -13,37 +13,7 @@
       <div class="tab-swiper vux-center content">
         <scroller :height="getScreenHeight()-44+'px'" lock-x scroller-y v-ref:zx>
           <div>
-              <div v-for="order in zxList" class="order">
-                <div class="zc-line-1">
-                    <div class="zc-user-name">{{order.customerName}}</div>
-                    <div class="zc-user-address">{{order.orderLocation+order.orderAddress}}</div>
-                    <div class="zc-user-more" v-tap="viewDetail('zx',order.orderNo)">查看详情</div>
-                  </div>
-                  <div class="zc-line-2">
-                    <div class="zc-order-date"><img src="./time.png">{{getTime(order.orderTime)}}</div>
-                    <div class="zc-order-status">{{zxStatusList[order.status].name}}</div>
-                  </div>
-                  <div class="zc-line-3" v-if="order.manager.nickname">
-                    <div class="zc-butler-img"><img :src="order.manager.profileImage"></div>
-                    <div class="zc-butler-name">{{order.manager.nickname}}</div>
-                    <div class="zc-butler-tel" onclick="location.href='tel:{{order.manager.mobile}}'"><img src="./tel.png"></div>
-                  </div>
-                  <div class="zc-line-3" v-if="order.projectManager.nickname">
-                    <div class="zc-butler-img"><img :src="order.projectManager.profileImage"></div>
-                    <div class="zc-butler-name">{{order.projectManager.nickname}}</div>
-                    <div class="zc-butler-tel" onclick="location.href='tel:{{order.projectManager.mobile}}'"><img src="./tel.png"></div>
-                  </div>
-                  <div class="zc-line-3" v-if="order.planList[0].foreman.nickname">
-                    <div class="zc-butler-img"><img :src="order.planList[0].foreman.profileImage"></div>
-                    <div class="zc-butler-name">{{order.planList[0].foreman.nickname}}</div>
-                    <div class="zc-butler-tel" onclick="location.href='tel:{{order.planList[0].foreman.mobile}}'"><img src="./tel.png"></div>
-                  </div>
-                  <div class="zc-line-3" v-if="order.planList.length===2">
-                    <div class="zc-butler-img"><img :src="order.planList[1].foreman.profileImage"></div>
-                    <div class="zc-butler-name">{{order.planList[1].foreman.nickname}}</div>
-                    <div class="zc-butler-tel" onclick="location.href='tel:{{order.planList[1].foreman.mobile}}'"><img src="./tel.png"></div>
-                  </div>
-              </div>
+            <j-zx-order-item :data="order" role="user" v-for="order in zxList"></j-zx-order-item>
           </div>
           <div v-if="zxList.length==0">
             <div class="no-data"><img src="no-data.png"><span>暂无订单</span></div>
@@ -67,25 +37,7 @@
       <div class="tab-swiper vux-center content">
         <scroller :height="getScreenHeight()-44+'px'" lock-x scroller-y v-ref:tk>
           <div>
-
-            <div class="order" v-for="order in tkList">
-              <div class="zc-line-1">
-                <div class="zc-user-name">{{order.customerName}}</div>
-                <!-- <div class="zc-user-address">{{order.userAddress}}</div> -->
-              </div>
-              <div class="zc-line-2">
-                <div class="zc-order-shop">{{order.store.name}}</div>
-                <div class="zc-order-status">{{zcStatusList[order.status].name}}</div>
-              </div>
-              <div class="zc-line-3">
-                <div class="zc-butler-img"><img :src="order.manager.profileImage"></div>
-                <div class="zc-butler-name">{{order.manager.nickname}}</div>
-                <div class="zc-butler-tel" onclick="location.href='tel:{{order.manager.mobile}}'"><img src="tel.png"></div>
-              </div>
-              <div class="zc-line-4" v-if="order.status > 0">
-                <div class="zc-count">退款金额<span>{{order.customerRefundAmount|currency "￥" 2}}</span></div>
-              </div>
-            </div>
+            <j-tk-order-item :data="order" role="user" v-for="order in tkList"></j-tk-order-item>
           </div>
           <div v-if="tkList.length==0">
             <div class="no-data"><img src="no-data.png"><span>暂无订单</span></div>
@@ -107,6 +59,8 @@ import Swiper from 'vux-components/swiper'
 import SwiperItem from 'vux-components/swiper-item'
 import Scroller from 'vux-components/scroller'
 import JZcOrderItem from 'common/components/j-zc-order-item.vue'
+import JZxOrderItem from 'common/components/j-zx-order-item.vue'
+import JTkOrderItem from 'common/components/j-tk-order-item.vue'
 import axios from 'axios'
 try{
   axios.defaults.headers.common['x-user-token'] = JSON.parse(localStorage.getItem("user")).token
@@ -118,34 +72,6 @@ export default {
   data() {
     return {
       index: 0,
-      zxStatusList: [{
-        status: 0,
-        name: "订单已删除"
-      }, {
-        status: 1,
-        name: "已预约"
-      }, {
-        status: 2,
-        name: "已上门"
-      }, {
-        status: 3,
-        name: "待选方案"
-      }, {
-        status: 4,
-        name: "待支付"
-      }, {
-        status: 5,
-        name: "待施工"
-      }, {
-        status: 6,
-        name: "施工中"
-      }, {
-        status: 7,
-        name: "已完工"
-      }, {
-        status: 8,
-        name: "订单已取消"
-      }],
       zxList: [],
       zcList: [],
       tkList: []
@@ -157,7 +83,9 @@ export default {
     Swiper,
     SwiperItem,
     Scroller,
-    JZcOrderItem
+    JZcOrderItem,
+    JZxOrderItem,
+    JTkOrderItem
   },
   ready() {
     let suc_count = 0
@@ -205,16 +133,6 @@ export default {
   methods: {
     getScreenHeight() {
       return document.body.clientHeight
-    },
-    getTime(timeStamp) {
-      var d = new Date(timeStamp*1000);
-      var Y = d.getFullYear() + '-';
-      var M = (d.getMonth() + 1 < 10 ? '0' + (d.getMonth() + 1) : d.getMonth() + 1) + '-';
-      var D = (d.getDate() < 10 ? '0' + (d.getDate()) : d.getDate());
-      return Y + M + D
-    },
-    viewDetail(type, orderNo) {
-      eval(`window.location.href='${type}-order.html?orderNo=${orderNo}'`)
     }
   }
 }
@@ -245,126 +163,6 @@ header {
 .tab-active {
     color: #88C929 !important;
     border-color: #88C929 !important;
-}
-.order {
-    width: 100%;
-    height: auto;
-    background-color: #fff;
-    margin-bottom: 10px;
-    .zc-line-1 {
-        height: 50px;
-        width: 100%;
-        border-bottom: 1px solid #eee;
-        position: relative;
-        .zc-user-name {
-            position: absolute;
-            top: 6px;
-            left: 15px;
-            font-size: 16px;
-            color: #393939;
-        }
-        .zc-user-address {
-            position: absolute;
-            bottom: 6px;
-            left: 15px;
-            font-size: 12px;
-            color: #393939;
-        }
-        .zc-user-more {
-            position: absolute;
-            top: 19px;
-            right: 15px;
-            font-size: 12px;
-            color: #3ba794;
-        }
-    }
-    .zc-line-2 {
-        height: 40px;
-        width: 100%;
-        border-bottom: 1px solid #eee;
-        position: relative;
-        .zc-order-date {
-            position: absolute;
-            top: 14px;
-            left: 15px;
-            font-size: 12px;
-            color: #393939;
-            img {
-                vertical-align: middle;
-                height: 12px;
-                width: 12px;
-                margin-right: 5px;
-            }
-        }
-        .zc-order-shop {
-            position: absolute;
-            top: 0;
-            left: 15px;
-            height: 40px;
-            line-height: 40px;
-            font-size: 14px;
-            color: #393939;
-        }
-        .zc-order-status {
-            position: absolute;
-            right: 15px;
-            top: 15px;
-            font-size: 12px;
-            color: #393939;
-        }
-    }
-    .zc-line-3 {
-        height: 50px;
-        width: 100%;
-        border-bottom: 1px solid #eee;
-        position: relative;
-        .zc-butler-img {
-            position: absolute;
-            top: 5px;
-            left: 15px;
-            height: 40px;
-            width: 40px;
-            border-radius: 20px;
-            img {
-                height: 100%;
-                width: 100%;
-                border-radius: 50%;
-            }
-        }
-        .zc-butler-name {
-            position: absolute;
-            bottom: 15px;
-            left: 65px;
-            font-size: 16px;
-            color: #393939;
-        }
-        .zc-butler-tel {
-            position: absolute;
-            top: 15px;
-            right: 15px;
-            height: 20px;
-            width: 20px;
-            img {
-                height: 100%;
-                width: 100%;
-            }
-        }
-    }
-    .zc-line-4 {
-        position: relative;
-        height: 20px;
-        width: 100%;
-        .zc-count {
-            position: absolute;
-            right: 15px;
-            top: 4px;
-            font-size: 12px;
-            color: #393939;
-            span {
-                color: #EC5835;
-            }
-        }
-    }
 }
 .no-data {
     position: relative;
