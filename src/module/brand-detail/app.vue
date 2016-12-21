a84fd231ebac0fd81963ba45c874234e64f81541
mayanxiao <yutouleetown@gmail.com>
2016-12-21 18:05:55 +0800

    authorize change

--- src/module/brand-detail/app.vue
+++ src/module/brand-detail/app.vue
@@ -37,7 +37,7 @@ import bgImg from './bg.png'
 try {
   let now = Number(new Date().getTime())
   if (Number(JSON.parse(localStorage.user).expiredAt) < now
-    ||!JSON.parse(localStorage.user).profile.mobile
+    // ||!JSON.parse(localStorage.user).profile.mobile
     ) {
     localStorage.removeItem('user')
     location.href = './wxAuth.html?url=' + encodeURIComponent(location.href)
